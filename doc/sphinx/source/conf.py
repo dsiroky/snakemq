c12e062c8c5d9e4091dc73ba8a4d32e9d4963d46
David Siroky <siroky@dasir.cz>
2015-03-28 09:01:15 +0100

    update years

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'snakeMQ'
-copyright = u'2011, David Siroky'
+copyright = u'2015, David Siroky'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
